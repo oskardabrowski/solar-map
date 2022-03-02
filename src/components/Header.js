7593d9c52eba8740232c4b4059ac2f15b749056c
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-03-02 22:44:06 +0100

    Adding new logo and commit new changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import AppLogo from '../images/SolarnaMapaLogo.svg';
+import AppLogo from '../images/SolarMapLogo.svg';
 import UnivesityLogo from '../images/UMKAkronimLogo.svg';
 
 const Head = styled.header`
