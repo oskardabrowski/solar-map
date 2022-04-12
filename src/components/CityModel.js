b9cd7ec3dcf713f753ffedadb0d540fceb5bafc8
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-04-12 18:17:59 +0200

    Adding next models to 3D scene

--- src/components/CityModel.js
+++ src/components/CityModel.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { N37, N36, N35_1, N35_2, N34, N33, N32, N31, N30, N29, N28 } from './Single3DModels/Surface'
+import { N50, N49, N48, N47, N46, N45, N44, N43, N42, N41, N40, N39, N38, N37, N36, N35_1, N35_2, N34, N33, N32, N31, N30, N29, N28 } from './Single3DModels/Surface'
 
 const CityModel = () => {
   return (
     <>
+    <N50 />
+    <N49 />
+    <N48 />
+    <N47 />
+    <N46 />
+    <N45 />
+    <N44 />
+    <N43 />
+    <N42 />
+    <N41 />
+    <N40 />
+    <N39 />
+    <N38 />
     <N37 />
     <N36 />
     <N35_1 />
--- src/components/Single3DModels/Surface.js
+++ src/components/Single3DModels/Surface.js
@@ -2,6 +2,111 @@ import React from 'react'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from '@react-three/fiber';
 
+export const N50 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n50.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ -67, -0.65, 131]} />
+    </>
+  )
+}
+export const N49 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n49.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 187.5, 0, 103]} />
+    </>
+  )
+}
+export const N48 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n48.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 154, -0.75, 103]} />
+    </>
+  )
+}
+export const N47 = () => {
+    {/*Popraw model*/}
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n47.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 45, -0.3, 100]} />
+    </>
+  )
+}
+export const N46 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n46.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1.05} position={[ -5, -0.1, 91.5]} />
+    </>
+  )
+}
+export const N45 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n45.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1.05} position={[ -56.5, 0, 104.5]} />
+    </>
+  )
+}
+export const N44 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n44.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1.05} position={[ 250, -0.25, 46]} />
+    </>
+  )
+}
+export const N43 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n43.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 186, 0, 51]} />
+    </>
+  )
+}
+export const N42 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n42.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 130, 0, 44.15]} />
+    </>
+  )
+}
+export const N41 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n41.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 85.5, -0.75, 48]} />
+    </>
+  )
+}
+export const N40 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n40.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[ 42.5, 0, 53.5]} />
+    </>
+  )
+}
+export const N39 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n39.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[-7, 0, 49]} />
+    </>
+  )
+}
+export const N38 = () => {
+  const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n38.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1} position={[-46, 0, 51.5]} />
+    </>
+  )
+}
 export const N37 = () => {
   const gltf = useLoader(GLTFLoader, "./Models/SurfaceFragments/n37.gltf");
   return (
