0a5569f993446566cd5389f1fa8ec698d4bf5207
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2022-05-30 20:15:10 +0200

    Adding solar panel as lime polygon, problem with single editing

--- src/components/Geoman.js
+++ src/components/Geoman.js
@@ -15,25 +15,56 @@ const Geoman = () => {
   const { tools, solarPanelDrawing, setSolarPanelDrawing } =
     useContext(MapContext);
   const map = context.layerContainer || context.map;
+  const layers = map.pm.getGeomanLayers();
+
+  useEffect(() => {
+    map.pm.getGeomanLayers().forEach((layer) => {
+      if (layer.hasOwnProperty("SolarPanel") && layer.SolarPanel === true) {
+        layer.setStyle({ color: "lime", fillColor: "lime", fillOpacity: "1" });
+      }
+    });
+  }, [map.pm.getGeomanLayers()]);
 
   map.on("pm:create", function (e) {
     e.layer.showMeasurements();
-    // console.log(solarPanelDrawing);
-    // e.layer.solarPanel = solarPanelDrawing;
-    if (solarPanelDrawing === true) {
-      e.layer.solarPanel = true;
-      e.layer.setStyle({ color: "green" });
-    }
+    e.layer.SolarPanel = solarPanelDrawing;
 
-    console.log(e.layer);
     setSolarPanelDrawing(false);
-    console.log(solarPanelDrawing);
-    e.layer.on("pm:edit", function (x) {});
+    // e.layer.on("pm:edit", function ({ layer }) {
+    //   console.log(layer.toGeoJSON());
+    // });
   });
 
-  map.on("pm:drawend", function (e) {
-    setSolarPanelDrawing(false);
-  });
+  // map.on("pm:edit", function (e) {
+  //   e.layer.forEach((el) => {
+  //     console.log(el);
+  //   });
+  // });
+
+  // map.on("pm:drawend", function (e) {
+  //   setSolarPanelDrawing(false);
+  // });
+
+  // useEffect(() => {
+  //   if (solarPanelDrawing === true) {
+  //     map.pm.getGeomanLayers().forEach((el) => {
+  //       if (el.SolarPanel == true) {
+  //         el.setStyle({ pmIgnore: false });
+  //       } else {
+  //         el.setStyle({ pmIgnore: true });
+  //       }
+  //     });
+  //   }
+  //   if (solarPanelDrawing === false) {
+  //     map.pm.getGeomanLayers().forEach((el) => {
+  //       if (el.SolarPanel === false) {
+  //         el.setStyle({ pmIgnore: false });
+  //       } else {
+  //         el.setStyle({ pmIgnore: true });
+  //       }
+  //     });
+  //   }
+  // }, [solarPanelDrawing]);
 
   useEffect(() => {
     if (!tools.includes("DrawTools")) {
@@ -87,78 +118,78 @@ const Geoman = () => {
     //   e.layer.on("pm:edit", function (x) {});
     // });
 
-    map.on("pm:create", (e) => {
-      // e.layer.showMeasurements();
-      // if (e.layer && e.layer.pm) {
-      //   const shape = e;
-      //   // shape.layer.pm.enable();
-      //   shape.layer.showMeasurements();
-      //   console.log(shape.layer);
-      //   console.log(leafletContainer.pm.getGeomanLayers());
-      //   // leafletContainer.each;
-      //   leafletContainer.pm.getGeomanLayers().map((layer, index) => {
-      //     // layer.showMeasurements();
-      //     // console.log(layer);
-      //     if (e.shape === "Line") {
-      //       // layer.showMeasurements();
-      //     } else if (e.shape === "Polygon" || e.shape === "Rectangle") {
-      //       layer
-      //         .bindPopup(
-      //           `${turf.area(e.layer.toGeoJSON()).toFixed(2)} m${"2".sup()}`,
-      //           {
-      //             autoPan: false,
-      //           }
-      //         )
-      //         .openPopup();
-      //       // .showMeasurements();
-      //       layer.eachLayer(function (lr) {
-      //         lr.showMeasurements();
-      //       });
-      //     } else if (e.shape === "Circle") {
-      //       const latlng = e.layer._latlng;
-      //       const radius = e.layer.options.radius;
-      //       const circle = turf.circle([latlng.lat, latlng.lng], radius);
-      //       layer
-      //         .bindPopup(
-      //           `${(turf.area(circle) / 1_000_000).toFixed(2)} m${"2".sup()}`,
-      //           {
-      //             autoPan: false,
-      //           }
-      //         )
-      //         .openPopup();
-      //       // .showMeasurements();
-      //     }
-      //   });
-      //   // leafletContainer.pm.getGeomanLayers().map((layer, index) => {});
-      //   shape.layer.on("pm:edit", (e) => {
-      //     // if (e.shape === "Line") {
-      //     //   e.layer.bindPopup(
-      //     //     `${(turf.length(e.layer.toGeoJSON()) * 1000).toFixed(2)} m`,
-      //     //     {
-      //     //       autoPan: false,
-      //     //     }
-      //     //   );
-      //     // } else if (e.shape === "Polygon" || e.shape === "Rectangle") {
-      //     //   e.layer.bindPopup(
-      //     //     `${turf.area(e.layer.toGeoJSON()).toFixed(2)} m2`,
-      //     //     {
-      //     //       autoPan: false,
-      //     //     }
-      //     //   );
-      //     // } else if (e.shape === "Circle") {
-      //     //   const latlng = e.layer._latlng;
-      //     //   const radius = e.layer._mRadius;
-      //     //   const circle = turf.circle([latlng.lat, latlng.lng], radius);
-      //     //   e.layer.bindPopup(
-      //     //     `${(turf.area(circle) / 1_000_000).toFixed(2)} m2`,
-      //     //     {
-      //     //       autoPan: false,
-      //     //     }
-      //     //   );
-      //     // }
-      //   });
-      // }
-    });
+    // map.on("pm:create", (e) => {
+    //   // e.layer.showMeasurements();
+    //   // if (e.layer && e.layer.pm) {
+    //   //   const shape = e;
+    //   //   // shape.layer.pm.enable();
+    //   //   shape.layer.showMeasurements();
+    //   //   console.log(shape.layer);
+    //   //   console.log(leafletContainer.pm.getGeomanLayers());
+    //   //   // leafletContainer.each;
+    //   //   leafletContainer.pm.getGeomanLayers().map((layer, index) => {
+    //   //     // layer.showMeasurements();
+    //   //     // console.log(layer);
+    //   //     if (e.shape === "Line") {
+    //   //       // layer.showMeasurements();
+    //   //     } else if (e.shape === "Polygon" || e.shape === "Rectangle") {
+    //   //       layer
+    //   //         .bindPopup(
+    //   //           `${turf.area(e.layer.toGeoJSON()).toFixed(2)} m${"2".sup()}`,
+    //   //           {
+    //   //             autoPan: false,
+    //   //           }
+    //   //         )
+    //   //         .openPopup();
+    //   //       // .showMeasurements();
+    //   //       layer.eachLayer(function (lr) {
+    //   //         lr.showMeasurements();
+    //   //       });
+    //   //     } else if (e.shape === "Circle") {
+    //   //       const latlng = e.layer._latlng;
+    //   //       const radius = e.layer.options.radius;
+    //   //       const circle = turf.circle([latlng.lat, latlng.lng], radius);
+    //   //       layer
+    //   //         .bindPopup(
+    //   //           `${(turf.area(circle) / 1_000_000).toFixed(2)} m${"2".sup()}`,
+    //   //           {
+    //   //             autoPan: false,
+    //   //           }
+    //   //         )
+    //   //         .openPopup();
+    //   //       // .showMeasurements();
+    //   //     }
+    //   //   });
+    //   //   // leafletContainer.pm.getGeomanLayers().map((layer, index) => {});
+    //   //   shape.layer.on("pm:edit", (e) => {
+    //   //     // if (e.shape === "Line") {
+    //   //     //   e.layer.bindPopup(
+    //   //     //     `${(turf.length(e.layer.toGeoJSON()) * 1000).toFixed(2)} m`,
+    //   //     //     {
+    //   //     //       autoPan: false,
+    //   //     //     }
+    //   //     //   );
+    //   //     // } else if (e.shape === "Polygon" || e.shape === "Rectangle") {
+    //   //     //   e.layer.bindPopup(
+    //   //     //     `${turf.area(e.layer.toGeoJSON()).toFixed(2)} m2`,
+    //   //     //     {
+    //   //     //       autoPan: false,
+    //   //     //     }
+    //   //     //   );
+    //   //     // } else if (e.shape === "Circle") {
+    //   //     //   const latlng = e.layer._latlng;
+    //   //     //   const radius = e.layer._mRadius;
+    //   //     //   const circle = turf.circle([latlng.lat, latlng.lng], radius);
+    //   //     //   e.layer.bindPopup(
+    //   //     //     `${(turf.area(circle) / 1_000_000).toFixed(2)} m2`,
+    //   //     //     {
+    //   //     //       autoPan: false,
+    //   //     //     }
+    //   //     //   );
+    //   //     // }
+    //   //   });
+    //   // }
+    // });
 
     map.on("pm:remove", (e) => {});
 
